aec6ae284f03d1d6d7c17ac3966318a38c6a45fc
drakon <polish1.david@gmail.com>
2020-08-01 12:10:48 +0200

    Add Models

--- ServiceCompany/viewer/models.py
+++ ServiceCompany/viewer/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.db.models import Model, CharField
 
-# Create your models here.
+
+class Client(Model):
+    name = CharField(max_length=128)
+    surname = CharField(max_length=128)
+    phone_number = CharField(max_length=128)
+    adress = CharField(max_length=128)
+    email = CharField(max_length=128)
+
+
+    def __str__(self):
+        return self.name +" "+ self.surname + " " + self.adress
+
+class Service(Model):
+    name = CharField(max_length=128)
+    surname = CharField(max_length=128)
+    specialization = CharField(max_length=128)
+
+
+    def __str__(self):
+        return self.name +" "+ self.surname + " : " + self.specialization
